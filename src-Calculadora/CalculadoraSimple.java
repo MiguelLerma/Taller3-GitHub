3f6a5d706296068516d8cad2dc50589e930ffdfc
1622290-3744 <benavides.alejandro@correounivalle.edu.co>
2017-04-27 10:21:20 -0500

    Se agrego la multiplicacion y division

--- src-Calculadora/CalculadoraSimple.java
+++ src-Calculadora/CalculadoraSimple.java
@@ -131,7 +131,7 @@ public class CalculadoraSimple extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btn_sumaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_sumaActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_btn_sumaActionPerformed
 
     private void btn_restaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_restaActionPerformed
@@ -139,11 +139,19 @@ public class CalculadoraSimple extends javax.swing.JFrame {
     }//GEN-LAST:event_btn_restaActionPerformed
 
     private void btn_multiplicacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_multiplicacionActionPerformed
-        // TODO add your handling code here:
+        try{
+        txt_resultado.setText(Integer.toString(Integer.parseInt(txt_valor1.getText().toString())*Integer.parseInt(txt_valor2.getText().toString())));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }//GEN-LAST:event_btn_multiplicacionActionPerformed
 
     private void btn_divisionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_divisionActionPerformed
-        // TODO add your handling code here:
+        try{
+        txt_resultado.setText(Integer.toString(Integer.parseInt(txt_valor1.getText().toString())/Integer.parseInt(txt_valor2.getText().toString())));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }//GEN-LAST:event_btn_divisionActionPerformed
 
     /**
