626a04acc0d76d401601e567371f3e77d0c39b00
Miguel Lerma <miguel.lerma@correounivalle.edu.co>
2017-05-18 14:50:52 -0500

    Adicionar nombres

--- src-Calculadora/CalculadoraSimple.java
+++ src-Calculadora/CalculadoraSimple.java
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Integrantes:
+//Miguel Angel Lerma
+//Julian Campiño
+//Alejandro Martinez
+
 package tallergitnetbeans;
 
 
